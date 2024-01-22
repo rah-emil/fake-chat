b60373a90235405af8d0cc3b9e95c5e84ec6f74d
Rah Emil <013131@mail.ru>
2024-01-22 20:14:57 +0200

    feat: base functional for plugin (posting messages)

--- src/fake-chat.ts
+++ src/fake-chat.ts
@@ -1,18 +1,66 @@
+export interface FakeChatMessage {
+  /**
+   * Text of message
+   */
+  text: string;
+
+  /**
+   * Posting time after the previous one
+   */
+  delay: number;
+
+  /**
+   * Class names of message
+   */
+  className?: string;
+}
+
 export interface FakeChatOptions {
-  username: string;
+  /**
+   * The order of adding messages to the wrapper
+   */
+  position?: InsertPosition;
+
+  /**
+   * Array of messages to publish
+   */
+  messages: FakeChatMessage[];
 }
 
 export class FakeChat {
+  private wrapper: HTMLElement | null;
   private options: FakeChatOptions;
 
-  constructor(options: FakeChatOptions) {
+  constructor(el: string, options: FakeChatOptions) {
+    this.wrapper = document.querySelector(el);
     this.options = options;
+
+    this._init();
   }
 
-  test() {
-    console.log('options', this.options);
+  private _init() {
+    const ms = this.options.messages[0].delay ?? 1000;
+    this._appendMessage(ms);
+  }
+
+  private _appendMessage(ms: number) {
+    setTimeout(() => {
+      const messageWrapper = document.createElement('div');
+      const messageContent = document.createTextNode(this.options.messages[0].text);
+
+      messageWrapper.appendChild(messageContent);
+
+      try {
+        this.wrapper?.insertAdjacentElement(this.options.position ?? 'afterend', messageWrapper);
+        this.options.messages.splice(0, 1);
+
+        if(this.options.messages.length !== 0) {
+          const ms = this.options.messages[0].delay ?? 1000;
+          this._appendMessage(ms);
+        }
+      } catch(err) {
+        console.warn(err);
+      }
+    }, ms);
   }
 }
-
-
-
